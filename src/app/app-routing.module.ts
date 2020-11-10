2b4f347b96878d75f314991efa1a548097c166a7
Garache10 <joseangelgarachesaenz@gmail.com>
2020-11-09 22:29:01 -0600

    Avanzando en navegacion y nuevos componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+import { ProfesorComponent } from './profesor/profesor.component';
+import { EstudianteComponent } from './estudiante/estudiante.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path:'user',component:UserComponent }
+  { path:'user',component:UserComponent },
+  { path:'adm', component:AdminComponent },
+  { path:'prf', component:ProfesorComponent },
+  { path:'est', component:EstudianteComponent },
+  { path:'login', component:LoginComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { ShowAllComponent } from './user/show-all/show-all.component';
 import { AddEditComponent } from './user/add-edit/add-edit.component';
 
-//Componentes del proyecto
+//Componentes rol Admin
 import { AdminComponent } from './admin/admin.component';
-import { SharedService } from './shared.service';
+import { AdminHomeComponent } from './admin/admin-home/admin-home.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//Componentes rol Profesor
+import { ProfesorComponent } from './profesor/profesor.component';
+import { ProfesorHomeComponent } from './profesor/profesor-home/profesor-home.component';
+
+//Componentes rol Estudiante
+import { EstudianteComponent } from './estudiante/estudiante.component';
+import { EstudianteHomeComponent } from './estudiante/estudiante-home/estudiante-home.component';
+
+//Componentes del proyecto
+import { SharedService } from './shared.service';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
@@ -21,7 +33,13 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     UserComponent,
     ShowAllComponent,
     AddEditComponent,
-    AdminComponent
+    AdminComponent,
+    AdminHomeComponent,
+    ProfesorComponent,
+    EstudianteComponent,
+    ProfesorHomeComponent,
+    EstudianteHomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
