a4586a99be413bbd651ac1a60694134022e5b049
Garache10 <joseangelgarachesaenz@gmail.com>
2020-11-09 22:37:09 -0600

    continuando navegacion

---
+++ src/app/estudiante/estudiante-home/estudiante-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante-home',
+  templateUrl: './estudiante-home.component.html',
+  styleUrls: ['./estudiante-home.component.css']
+})
+export class EstudianteHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
